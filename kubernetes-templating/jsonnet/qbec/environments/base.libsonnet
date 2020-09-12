2522848c52282877ceb2e39b2d836ef28082914b
Anton Yakovlev <sybertuk@gmail.com>
2020-09-12 22:25:54 +0300

    god bless all follow-up tests

---
+++ kubernetes-templating/jsonnet/qbec/environments/base.libsonnet
@@ -0,0 +1,9 @@
+
+// this file has the baseline default parameters
+{
+  components: {
+    currency: {
+      Port: 7001,
+    },
+  },
+}
---
+++ kubernetes-templating/terraform/main.tf
@@ -0,0 +1,33 @@
+provider "google" {
+  project = "hazel-service-283709"
+  region  = "europe-west3"
+  zone    = "europe-west3-a"
+}
+
+resource "google_container_cluster" "primary" {
+  name     = "my-gke-cluster"
+  location = "europe-west3"
+
+  # We can't create a cluster with no node pool defined, but we want to only use
+  # separately managed node pools. So we create the smallest possible default
+  # node pool and immediately delete it.
+  remove_default_node_pool = true
+  initial_node_count       = 1
+}
+
+resource "google_container_node_pool" "primary_preemptible_nodes" {
+  name       = "my-node-pool"
+  location   = "europe-west3"
+  cluster    = google_container_cluster.primary.name
+  node_count = 1
+
+  node_config {
+    preemptible  = true
+    machine_type = "e2-medium"
+
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
+  }
+}
